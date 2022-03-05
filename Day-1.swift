3c3896f6aff03f49afb1ebc956596bde345aa9ef
JonathasLopes <jon.athasmoreira@hotmail.com>
2022-03-05 02:18:48 -0300

    Initial Commit

---
+++ Day-1.swift
@@ -0,0 +1,32 @@
+//  Created by Jonathas Lopes on 05/03/22.
+
+import Foundation
+
+// String
+var str = "Hello"
+
+// Integer
+var int = 10
+var big = 12_000_000 //: You can use '_' to say that this number is a high number like 12.000.000, you change the dots to underline!
+
+// Double
+var doub = 10.0
+
+// Boolean
+var bool = true
+
+// Constant (It is recommended to use)
+let const = "Nice!"
+
+// Interpolate String
+var interp = "This is an interpolate: \(str)"
+
+// Multi-line String
+var multi = """
+This is a
+multi-line example!
+"""
+
+// Type Annotations (Used to say what kind of variable is and you can use to a empty string too)
+var age: Int = 10
+var name: String = "" //: You can set a string after if you want. However, if you try to put 'var name = ""', it will show you an error!
